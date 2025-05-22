541633d0104ce5b6a21cd83a132db9ff1cd4d680
Burhan Mustafa <burhanmustafa@Burhans-MacBook-Air.local>
2025-05-22 04:42:35 -0400

    Initial Commit

---
+++ src/sportsinventory/SportsInventory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sportsinventory;
+
+/**
+ *
+ * @author User
+ */
+public class SportsInventory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
